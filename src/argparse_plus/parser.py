311162bd1c9bbb4dbb05eadd148bb10312b3f049
antonio <a.cavallo@cavallinux.eu>
2022-07-23 19:47:04 -0400

    udpate typing info

--- src/argparse_plus/parser.py
+++ src/argparse_plus/parser.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import argparse
-from typing import Sequence
+from typing import Sequence, Optional, Tuple, Dict, Any
 
 from . import tools
 
@@ -24,12 +24,13 @@ class ArgumentParser(argparse.ArgumentParser):
         def __str__(self):
             return f"<{self.__class__.__name__} value={self.value} at {hex(id(self))}>"
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs):
         kwargs["formatter_class"] = get_formatter_class(kwargs.get("formatter_class"))
         super().__init__(*args, **kwargs)
         self.argument_default = ArgumentParser.NotAssigned(ArgumentParser.NotAssigned)
 
     def parse_known_args(
-        self, args: Sequence[str] | None = ..., namespace: argparse.Namespace | None = ...
+        self, args: Optional[Sequence[str]]=None, namespace: Optional[argparse.Namespace] = None
     ) -> tuple[argparse.Namespace, list[str]]:
+
         return super().parse_known_args(args, namespace)
