ae4b2c5f738cb6fe498f73c419a54af1fbae1a0a
antonio <a.cavallo@cavallinux.eu>
2022-07-24 12:56:18 -0400

    udpate

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 import contextlib
+import sys
 from unittest import mock
 from argparse_plus import parser
 import pytest
@@ -15,24 +16,6 @@ def cli():
         mcks["_print_message"] = stack.enter_context(
             mock.patch.object(ArgumentParser, "_print_message")
         )
-
-        # fix for changes between 3.9 -> 3.10
-        class MethodWrapper:
-            def __init__(self, fn):
-                self.fn = fn
-
-            def __call__(self, instance, *args, **kwargs):
-                from sys import version_info
-
-                if (version_info < (3, 10)) and args == ("options",) and not kwargs:
-                    args = ("positional arguments",)
-                return self.fn(instance, *args, **kwargs)
-
-        wrapper = MethodWrapper(ArgumentParser.add_argument_group)
-        mcks["_add_argument_group"] = stack.enter_context(
-            mock.patch.object(ArgumentParser, "add_argument_group", autospec=True)
-        )
-        mcks["_add_argument_group"].side_effect = wrapper
         yield Namespace(**mcks)
 
 
@@ -41,12 +24,15 @@ def test_cli_help(cli):
     p = parser.ArgumentParser(prog="abc.def", formatter_class="test")
     p.parse_args(["--help"])
 
+    txt = cli._print_message.call_args[0][0].strip()
+    if sys.version_info < (3, 10):
+        txt = txt.replace("\npositional arguments:\n", "\noptions:\n")
     assert (
-        cli._print_message.call_args[0][0].strip()
+        txt
         == """
 usage: abc.def [-h]
 
-positional arguments:
+options:
   -h, --help  show this help message and exit
 """.strip()
     )
